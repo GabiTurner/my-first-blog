9f4e0dfcae1b52599f77e7e9f63f79c7efd2dec7
Gabriela Turner <gabrielayes@hotmail.com>
2018-05-22 20:54:20 -0400

    My Django Girls app, first commit

---
+++ gitignore.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 22 20:34:16 2018
+
+@author: gabrielaturner
+"""
+
+*.pyc
+*~
+__pycache__
+myvenv
+db.sqlite3
+/static
+.DS_Store
